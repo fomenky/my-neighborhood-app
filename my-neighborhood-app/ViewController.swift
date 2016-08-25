bd8563ee4d122b93363374c060e9d7841de77e05
AceGod <acegod@acegods-mac.local>
2016-08-25 10:14:19 -0700

    Final Commit

--- my-neighborhood-app/ViewController.swift
+++ my-neighborhood-app/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        //tableView.estimatedRowHeight = 88.0 // Set row heigh limit
+        
+        DataService.instance.loadPosts()
+        
+        // Add Observer to NSNotification
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(onPostsLoaded), name: "postsLoaded", object: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = DataService.instance.loadedPosts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell!
+        {
+            cell.configureCell(post) // This glues EVERYTHING together!
+            return cell
+        }else{
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell;
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 88.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.loadedPosts.count
     }
 
-
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Implement if you want to show more details in another VC
+    }
+    
+    func onPostsLoaded (notif: AnyObject) {
+        tableView.reloadData()
+        
+    }
+    
 }
 
